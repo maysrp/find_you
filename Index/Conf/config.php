636196a99c4a6669d452382da8933d23cc9bc8e8
Egist <maysrp@gmail.com>
2016-11-18 01:29:55 +0800

    new

--- Index/Conf/config.php
+++ Index/Conf/config.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST'      =>  'localhost',     // 服务器地址  
     'DB_NAME'      =>  'ip',     // 数据库名  
     'DB_USER'      =>  'root',     // 用户名  
-    'DB_PWD'       =>  'ldn0741',     // 输入安装MySQL时设置的密码  
+    'DB_PWD'       =>  '',     // 输入安装MySQL时设置的密码  
     'DB_PORT'      =>  '3306',     // 端口  
     'DB_PREFIX'    =>  'think_',     // 数据库表前缀  
    // 'SHOW_PAGE_TRACE'=>True,
